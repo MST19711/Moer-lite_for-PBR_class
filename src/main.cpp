49face057a3de7c01720fb4b9734b34f8ed92b97
CX_Li <13041296428@163.com>
2024-06-24 16:33:15 +0800

    add parallel support

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 #define PBWIDTH 60
 
 std::mutex sampler_mtx;
-// #define PARALLEL
+#define PARALLEL
 
 #ifdef PARALLEL
 #define T_NUM 16
@@ -36,20 +36,18 @@ inline void printProgress(float percentage) {
     fflush(stdout);
 }
 
-void parallelRendering(int &x, int &width, int &y, int &height, int &spp,
+void parallelRendering(const int x, const int width, const int y,
+                       const int height, const int spp,
                        std::shared_ptr<Camera> &camera,
-                       std::shared_ptr<Sampler> &sampler,
-                       std::shared_ptr<Integrator> &integrator,
-                       std::shared_ptr<Scene> &scene) {
+                       const std::shared_ptr<Sampler> &sampler,
+                       const std::shared_ptr<Integrator> &integrator,
+                       const std::shared_ptr<Scene> &scene) {
     Vector2f NDC{(float)x / width, (float)y / height};
     Spectrum li(.0f);
     for (int i = 0; i < spp; ++i) {
-
-        sampler_mtx.lock();
         CameraSample samp = CameraSample{sampler->next2D()};
         Ray ray = camera->sampleRayDifferentials(samp, NDC);
         li += integrator->li(ray, *scene, sampler);
-        sampler_mtx.unlock();
     }
     camera->film->deposit({x, y}, li / spp);
 }
@@ -76,26 +74,13 @@ int main(int argc, char **argv) {
 
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-#ifdef PARALLEL
             if (T_queue.size() >= T_NUM) {
                 T_queue.front().join();
                 T_queue.pop();
             }
             T_queue.push(std::move(std::thread(
-                parallelRendering, std::ref(x), std::ref(width), std::ref(y),
-                std::ref(height), std::ref(spp), std::ref(camera),
+                parallelRendering, x, width, y, height, spp, std::ref(camera),
                 std::ref(sampler), std::ref(integrator), std::ref(scene))));
-
-#else
-            Vector2f NDC{(float)x / width, (float)y / height};
-            Spectrum li(.0f);
-            for (int i = 0; i < spp; ++i) {
-                Ray ray = camera->sampleRayDifferentials(
-                    CameraSample{sampler->next2D()}, NDC);
-                li += integrator->li(ray, *scene, sampler);
-            }
-            camera->film->deposit({x, y}, li / spp);
-#endif
             int finished = x + y * width;
             if (finished % 5 == 0) {
                 printProgress((float)finished / (height * width));
